218cfb6e5102f3be3c00e853bb21980364aa5258
Colin Houlette <153432267+Colin2005H@users.noreply.github.com>
2024-10-04 22:36:05 +0200

    Add form to fetch the postal code and display results

--- script/script.js
+++ script/script.js
@@ -3,18 +3,57 @@
 // 0: Array[insee code, name]
 // 1: ...
 // ]
+
 async function get_INSEE_code_from_CP(cp) {
-  request = "https://geo.api.gouv.fr/communes?codePostal=" + cp;
-  code_INSEE = [];
+  const request = "https://geo.api.gouv.fr/communes?codePostal=" + cp;
+  const code_INSEE = [];
   try {
-    promise = await fetch(request);
-    result = await promise.json();
-    for (i = 0; i < result.length; i++) {
+    const promise = await fetch(request);
+    const result = await promise.json();
+    for (let i = 0; i < result.length; i++) {
       code_INSEE[i] = [result[i].code, result[i].nom];
     }
-  } catch (e) {}
+  } catch (e) {
+    console.error("Erreur lors de la récupération du code INSEE :", e);
+  }
   return code_INSEE;
 }
+
+//fonction pour afficher les résultats dans le menu déroulant
+function displayResultsCities(codesInsee) {
+  const resultSelect = document.getElementById("resultSelect");
+  
+  resultSelect.innerHTML = "";
+
+  //si aucun code INSEE n'est trouvé
+  if (codesInsee.length === 0) {
+    const noResultOption = document.createElement("option");
+    noResultOption.textContent = "Aucun code INSEE trouvé pour ce code postal";
+    noResultOption.disabled = true; 
+    resultSelect.appendChild(noResultOption);
+  } else {
+    // Ajout des résultats sous forme d'options dans le select
+    codesInsee.forEach(([code, nom]) => {
+      const option = document.createElement("option");
+      option.value = code;
+      option.textContent = `${nom} : ${code}`;
+      resultSelect.appendChild(option);
+    });
+  }
+}
+
+// Ajout d'un gestionnaire d'événements pour le bouton "Valider"
+document.getElementById("buttonCodePostal").addEventListener("click", async function() {
+  const inputCodePostal = document.getElementById("inputCodePostal").value;
+
+  if (inputCodePostal) {
+    const codesInsee = await get_INSEE_code_from_CP(inputCodePostal);
+    displayResultsCities(codesInsee);  // Appel de la fonction pour afficher les résultats dans le select
+  } else {
+    console.error("Veuillez entrer un code postal valide.");
+  }
+});
+
 
 async function get_weather_info_from_insee_code(insee_code, day_from_today) {
   request = "https://api.meteo-concept.com/api/forecast/daily/" + day_from_today + "?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" + insee_code;
