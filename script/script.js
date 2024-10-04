4fa0ccbb7f7e4171273f6fbeb6f6084bcf365b97
pousone <albangruyer@gmail.com>
2024-10-04 19:32:58 +0200

    fix conflic

--- script/script.js
+++ script/script.js
@@ -9,13 +9,46 @@ async function get_INSEE_code_from_CP(cp) {
   try {
     promise = await fetch(request);
     result = await promise.json();
+    for (i = 0; i < result.length; i++) {
+      code_INSEE[i] = [result[i].code, result[i].nom];
+    }
   } catch (e) {}
-
-  for (i = 0; i < result.length; i++) {
-    code_INSEE[i] = [result[i].code, result[i].nom];
-  }
   return code_INSEE;
 }
+
+async function get_weather_info_from_insee_code(insee_code, day_from_today) {
+  request = "https://api.meteo-concept.com/api/forecast/daily/" + day_from_today + "?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" + insee_code;
+  weather_info = [];
+  try {
+    promise = await fetch(request);
+    result = await promise.json();
+    weather_info = result;
+  } catch (e) {}
+  
+  return weather_info;
+}
+
+async function get_location_info_from_insee_code(insee_code) {
+  request = "https://api.meteo-concept.com/api/location/city?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" + insee_code;
+  location_info = [];
+  try {
+    promise = await fetch(request);
+    result = await promise.json();
+    location_info = result;
+  } catch (e) {}
+  return location_info;
+}
+
+//for function test
+// window.onload = async function () {
+//   insee_code = await get_INSEE_code_from_CP(14123);
+//   console.log(insee_code);
+//   weather_info = await get_weather_info_from_insee_code(insee_code[0][0], 0);
+//   console.log(weather_info);
+//   location_info = await get_location_info_from_insee_code(insee_code);
+//   console.log(location_info);
+// };
+
 
 document
   .getElementById("buttonCodePostal")
@@ -25,80 +58,3 @@ document
     console.log(insee_code);
   });
 
-async function get_weather_info_from_insee_code(insee_code, day_from_today) {
-  request =
-    "https://api.meteo-concept.com/api/forecast/daily/" +
-    day_from_today +
-    "?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" +
-    insee_code;
-  weather_info = [];
-  try {
-    promise = await fetch(request);
-    result = await promise.json();
-  } catch (e) {}
-  weather_info = result;
-  return weather_info;
-}
-
-async function get_location_info_from_insee_code(insee_code) {
-  request =
-    "https://api.meteo-concept.com/api/location/city?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" +
-    insee_code;
-  location_info = [];
-  try {
-    promise = await fetch(request);
-    result = await promise.json();
-  } catch (e) {}
-  location_info = result;
-  return location_info;
-  request = "https://geo.api.gouv.fr/communes?codePostal=" + cp;
-  code_INSEE = [];
-  try {
-    promise = await fetch(request);
-    result = await promise.json();
-  } catch (e) {}
-
-  for (i = 0; i < result.length; i++) {
-    code_INSEE[i] = [result[i].code, result[i].nom];
-  }
-  return code_INSEE;
-}
-
-async function get_weather_info_from_insee_code(insee_code, day_from_today) {
-  request =
-    "https://api.meteo-concept.com/api/forecast/daily/" +
-    day_from_today +
-    "?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" +
-    insee_code;
-  weather_info = [];
-  try {
-    promise = await fetch(request);
-    result = await promise.json();
-  } catch (e) {}
-  weather_info = result;
-  return weather_info;
-}
-
-async function get_location_info_from_insee_code(insee_code) {
-  request =
-    "https://api.meteo-concept.com/api/location/city?token=7050a2dc76b480256fd4900fccf153567217d6f6fe483ed12f3af3e5dce6d687&insee=" +
-    insee_code;
-  location_info = [];
-  try {
-    promise = await fetch(request);
-    result = await promise.json();
-  } catch (e) {}
-  location_info = result;
-  return location_info;
-}
-
-//for function test
-
-window.onload = async function () {
-  insee_code = await get_INSEE_code_from_CP(14123);
-  console.log(insee_code);
-  weather_info = await get_weather_info_from_insee_code(insee_code[0][0], 0);
-  console.log(weather_info);
-  location_info = await get_location_info_from_insee_code(insee_code);
-  console.log(location_info);
-};
